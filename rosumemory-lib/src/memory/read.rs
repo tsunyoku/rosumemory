af4c8ab45e89ab6b8a2b6bb58d7cbb5bbf96a369
tsunyoku <mbruhyo@gmail.com>
2023-05-29 02:33:23 +0100

    feat: implement cross-platform memory reading

--- rosumemory-lib/src/memory/read.rs
+++ rosumemory-lib/src/memory/read.rs
@@ -3,17 +3,190 @@ use byteorder::ReadBytesExt;
 use sysinfo::Pid;
 use thiserror::Error;
 
-use crate::process::ProcessError;
-
 use super::MemoryMapping;
 
-pub(crate) fn enumerate_memory(
-    pid: Pid,
-    size: usize,
-    offset: Option<usize>,
-) -> Result<Vec<u8>, ProcessError> {
-    todo!("cross platform way to query memory pages")
+#[cfg(windows)]
+mod platform {
+    use std::os::windows::prelude::RawHandle;
+
+    use sysinfo::Pid;
+    use winapi::{
+        shared::{basetsd, minwindef},
+        um::{memoryapi, processthreadsapi, winnt},
+    };
+
+    use super::ReadMemoryError;
+
+    pub fn read_os_memory(
+        pid: Pid,
+        address: usize,
+        buffer: &mut [u8],
+    ) -> Result<(), ReadMemoryError> {
+        let process_handle = unsafe {
+            processthreadsapi::OpenProcess(
+                winnt::PROCESS_VM_READ,
+                0,
+                Into::<usize>::into(pid) as minwindef::DWORD,
+            )
+        };
+        if process_handle == (0 as RawHandle) {
+            return Err(ReadMemoryError::Unknown(
+                std::io::Error::last_os_error().to_string(),
+            ));
+        }
+
+        let result = unsafe {
+            memoryapi::ReadProcessMemory(
+                process_handle,
+                address as minwindef::LPVOID,
+                buffer.as_mut_ptr() as minwindef::LPVOID,
+                std::mem::size_of_val(buffer) as basetsd::SIZE_T,
+                std::ptr::null_mut(),
+            )
+        };
+        if result == 0 {
+            return Err(ReadMemoryError::Unknown(
+                std::io::Error::last_os_error().to_string(),
+            ));
+        }
+
+        Ok(())
+    }
 }
+
+#[cfg(target_os = "linux")]
+mod platform {
+    use libc::{c_void, iovec, pid_t, process_vm_readv};
+
+    pub fn read_os_memory(
+        pid: Pid,
+        address: usize,
+        buffer: &mut [u8],
+    ) -> Result<(), ReadMemoryError> {
+        let local_iov = iovec {
+            iov_base: buffer.as_mut_ptr() as *mut c_void,
+            iov_len: buffer.len(),
+        };
+
+        let remote_iov = iovec {
+            iov_base: address as *mut c_void,
+            iov_len: buffer.len(),
+        };
+
+        let result = unsafe {
+            process_vm_readv(
+                Into::<usize>::into(pid) as pid_t,
+                &local_iov,
+                1,
+                &remote_iov,
+                1,
+                0,
+            )
+        };
+        if result == -1 {
+            match std::io::Error::last_os_err().raw_os_error() {
+                Some(libc::ENOSYS) | Some(libc::EPERM) => {
+                    let procmem = fs::File::open(format!("/proc/{}/mem", Into::<usize>::into(pid)))
+                        .map_err(|e| ReadMemoryError::Unknown(e.to_string()))?;
+
+                    procmem
+                        .seek(io::SeekFrom::Start(address as u64))
+                        .map_err(|e| ReadMemoryError::Unknown(e.to_string()))?;
+
+                    procmem.read_exact(buffer);
+                }
+                _ => {
+                    return Err(ReadMemoryError::Unknown(
+                        std::io::Error::last_os_error().to_string(),
+                    ));
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(target_os = "macos")]
+mod platform {
+    use libc::{c_int, pid_t};
+    use mach::kern_return::{kern_return_t, KERN_SUCCESS};
+    use mach::port::{mach_port_name_t, mach_port_t, MACH_PORT_NULL};
+    use mach::vm_types::{mach_vm_address_t, mach_vm_size_t};
+
+    #[allow(non_camel_case_types)]
+    type vm_map_t = mach_port_t;
+    #[allow(non_camel_case_types)]
+    type vm_address_t = mach_vm_address_t;
+    #[allow(non_camel_case_types)]
+    type vm_size_t = mach_vm_size_t;
+
+    extern "C" {
+        fn vm_read_overwrite(
+            target_task: vm_map_t,
+            address: vm_address_t,
+            size: vm_size_t,
+            data: vm_address_t,
+            out_size: *mut vm_size_t,
+        ) -> kern_return_t;
+    }
+
+    fn task_for_pid(pid: Pid) -> std::io::Result<mach_port_name_t> {
+        if (Into::<usize>::into(pid) as pid_t) == unsafe { libc::getpid() } as pid_t {
+            return Ok(unsafe { mach::traps::mach_task_self() });
+        }
+
+        let mut task: mach_port_name_t = MACH_PORT_NULL;
+
+        unsafe {
+            let result = mach::traps::task_for_pid(
+                mach::traps::mach_task_self(),
+                Into::<usize>::into(pid) as c_int,
+                &mut task,
+            );
+            if result != KERN_SUCCESS {
+                return Err(io::Error::last_os_error());
+            }
+        }
+
+        Ok(task)
+    }
+
+    pub fn read_os_memory(
+        pid: Pid,
+        address: usize,
+        buffer: &mut [u8],
+    ) -> Result<(), ReadMemoryError> {
+        let task_pid = task_for_pid(pid).map_err(|e| ReadMemoryError::Unknown(e.to_string()))?;
+
+        let mut read_len = buffer.len() as vm_size_t;
+        let result = unsafe {
+            vm_read_overwrite(
+                task_pid,
+                address as vm_address_t,
+                buffer.len() as vm_size_t,
+                buffer.as_mut_ptr() as vm_address_t,
+                &mut read_len,
+            )
+        };
+
+        if read_len != buffer.len() as vm_size_t {
+            return Err(ReadMemoryError::Unknown(
+                "failed to read all memory".to_string(),
+            ));
+        }
+
+        if result != KERN_SUCCESS {
+            return Err(ReadMemoryError::Unknown(
+                std::io::Error::last_os_error().to_string(),
+            ));
+        }
+
+        Ok(())
+    }
+}
+
+use platform::read_os_memory;
 
 #[derive(Error, Debug)]
 pub enum ReadMemoryError {
@@ -24,7 +197,11 @@ pub enum ReadMemoryError {
 }
 
 pub fn read_memory(pid: Pid, address: usize, size: usize) -> Result<Vec<u8>, ReadMemoryError> {
-    todo!()
+    let mut data = vec![0; size];
+
+    read_os_memory(pid, address, &mut data)?;
+
+    Ok(data)
 }
 
 /// # Safety
@@ -34,7 +211,8 @@ pub unsafe fn structured_read_memory<T: MemoryMapping>(
     pid: Pid,
     address: usize,
 ) -> Result<T, ReadMemoryError> {
-    todo!()
+    let mut memory_region = read_memory(pid, address, T::size())?;
+    T::from_memory(pid.into(), memory_region.as_mut_ptr())
 }
 
 const MAX_STRING_LENGTH: u32 = 4096;
